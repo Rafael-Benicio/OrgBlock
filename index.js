f617a2dd6915680e2c1832cf11b71ae4a69553d8
Rafael-Benicio <jerobenicio@gmail.com>
2020-10-17 21:32:04 -0300

    New

---
+++ index.js
@@ -0,0 +1,49 @@
+const { app ,BrowserWindow , globalShortcut} = require('electron')
+const express=require('express')
+const apl=express()
+const cors=require('cors')
+const bodyParser=require('body-parser')
+
+apl.use(cors())
+apl.use(bodyParser.urlencoded({extended:false}))
+apl.use(bodyParser.json())
+// apl.use(express.json())
+
+function createWindow(){
+    const win =new BrowserWindow({
+        width:1080,
+        height:720,
+        title:"OrgBlock",
+        webPreferences:{
+            nodeIntegration:true,
+        }
+    })
+    win.loadURL('http://localhost:8888/')
+}
+
+app.whenReady().then(createWindow)
+
+
+
+apl.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/src/pages/Home/index.html")
+})
+
+apl.get('/Busca',(req,res)=>{
+    res.sendFile(__dirname+"/src/pages/Busca/index.html")
+})
+
+apl.get('/BuscaCard',(req,res)=>{
+    res.sendFile(__dirname+"/src/pages/BuscaCard/index.html")
+})
+
+apl.get('/CtCria',(req,res)=>{
+    res.sendFile(__dirname+"/src/pages/CreateCard/index.html")
+})
+
+apl.get('/GpCria',(req,res)=>{
+    res.sendFile(__dirname+"/src/pages/CreateGroup/index.html")
+})
+
+
+apl.listen(8888)
