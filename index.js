cd6c2729260d5c57f80902d7fd052cd8b97a5f0a
Rafael-Benicio <jerobenicio@gmail.com>
2020-10-18 15:30:48 -0300

    change

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const apl=express()
 const cors=require('cors')
 const bodyParser=require('body-parser')
 
+apl.use(express.static(__dirname+'/public'));
+
+
 apl.use(cors())
 apl.use(bodyParser.urlencoded({extended:false}))
 apl.use(bodyParser.json())
@@ -23,12 +26,6 @@ function createWindow(){
 
 app.whenReady().then(createWindow)
 
-
-
-apl.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/src/pages/Home/index.html")
-})
-
 apl.get('/Busca',(req,res)=>{
     res.sendFile(__dirname+"/src/pages/Busca/index.html")
 })
@@ -43,6 +40,18 @@ apl.get('/CtCria',(req,res)=>{
 
 apl.get('/GpCria',(req,res)=>{
     res.sendFile(__dirname+"/src/pages/CreateGroup/index.html")
+})
+
+apl.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/src/pages/Home/index.html")
+})
+
+apl.get('/Plan',(req,res)=>{
+    res.sendFile(__dirname+"/src/pages/Plan/index.html")
+})
+
+apl.get('/Planed',(req,res)=>{
+    res.sendFile(__dirname+"/src/pages/Planed/index.html")
 })
 
 
